f6b4b1381241ea4d17e51e660d0d88eb09c337a4
Anton Pichugin <anvlpi@yandex.ru>
2026-05-09 21:21:10 +0700

    Add comments, ID package

--- delivery_service.py
+++ delivery_service.py
@@ -1,29 +1,39 @@
+# ID успешной посылки: 161756950
 def main(robots_weights, capacity_limit) -> int:
     """Функция рассчета кол-ва платформ c Методом двух указателей"""
     robots_weights.sort()
-    platform_count: int = 0
+    platforms_count: int = 0
     left_point: int = 0
     right_point: int = len(robots_weights) - 1
-    temp_sum: int = 0
+    weight_temp_sum: int = 0
 
     while left_point <= right_point:
-        temp_sum = robots_weights[left_point] + robots_weights[right_point]
+        weight_temp_sum = (
+                            robots_weights[left_point] +
+                            robots_weights[right_point]
+        )
 
+        # Если указатели соприкоснулись - проверять не чего,
+        # просто добавляем платформы и прерываем
         if left_point == right_point:
-            platform_count += 1
+            platforms_count += 1
             break
+        # Если правый указатель с весом = грузоподъемности
+        # или вес слишком большой
         elif (
                 robots_weights[right_point] == capacity_limit or
-                temp_sum > capacity_limit
+                weight_temp_sum > capacity_limit
         ):
-            platform_count += 1
+            platforms_count += 1
             right_point -= 1
-        elif temp_sum <= capacity_limit:
-            platform_count += 1
+        # Если вес именно двух указателей меньше или равен,
+        # значит добавляем платформы
+        elif weight_temp_sum <= capacity_limit:
+            platforms_count += 1
             left_point += 1
             right_point -= 1
 
-    return platform_count
+    return platforms_count
 
 
 if __name__ == '__main__':
